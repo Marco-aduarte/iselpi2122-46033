86513b3c8d7c1604b4e2bd7f492edc74bf6cbf63
Marco <marcoantoniotd@gmail.com>
2021-10-13 09:51:41 +0100

    Finish a.List.js

--- hw4/aList.js
+++ hw4/aList.js
@@ -22,7 +22,6 @@ function listToArray(list){
     let idx = 0;
     let array = new Array();
     while(list != undefined){
-        console.log(list.value);
         array[idx] = list.value;
         list = list.rest;
         ++idx;
@@ -32,10 +31,21 @@ function listToArray(list){
 
 
 function prepend(element, list){
-
+    let l = new Object();
+    l.value = element;
+    l.rest = list;
+    return l;
 }
 
-console.log(JSON.stringify(arrayToList([10, 20])));     // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));    // [10, 20, 30]
-//console.log(prepend(10, prepend(20, null)));            // → {value: 10, rest: {value: 20, rest: null}}
-//console.log(nth(arrayToList([10, 20, 30]), 1));         // → 20*/
+function nth(list, number){
+    if(number == 0){
+        return list.value;
+    }
+    return nth(list.rest, --number);
+}
+
+console.log(JSON.stringify(arrayToList([10, 20])));             // → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));            // → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));                    // → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));                 // → 20
+console.log(nth(arrayToList([10, 20, 30, 40, 50]), 3));         // → 40

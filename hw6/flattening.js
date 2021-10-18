70dd1baab1cd0699672cb08fd94a0a312be5fa34
Marco <marcoantoniotd@gmail.com>
2021-10-18 12:52:08 +0100

    fix flattering

--- hw6/flattening.js
+++ hw6/flattening.js
@@ -1,25 +1,7 @@
 'use strict';
 
-function flatten(a, b, c, d){
-	if(a != undefined){
-		str = str.concat(a + ',' + b);
-	}
-	else {
-		str = str.concat(',' + b);
-	}
-}
-
-/*function flatten(previousValue, currentValue, currentIndex, array){
-	if(previousValue == undefined){
-		return;
-	}
-	else {
-		str = str.concat(array);
-	}
-}*/
-
 let arrays = [[1, 2, 3], [4, 5], [6]];
 let str = '[';
-arrays.reduce(flatten);
+arrays.reduce((previous, currentValue) => previous.concat(currentValue));
 str = str.concat(']');
 console.log(str);

e8817db25e88c5a075b9d0160acaf326651f17d8
Marco <marcoantoniotd@gmail.com>
2021-10-31 12:53:57 +0000

    get all properties of a game from a given id

--- PraticalAssignments/1stAssignment/lib/api.js
+++ PraticalAssignments/1stAssignment/lib/api.js
@@ -5,9 +5,28 @@ const fetch = require('node-fetch');
 const CLIENT_ID = process.env.ATLAS_CLIENT_ID;
 
 
+
+function getAllGames(url){
+    return fetch(url).then(res => res.text()).then(body => JSON.parse(body)).then(obj => obj.games);
+}
+
+function filter(propNames,obj){
+    if(propNames.id == obj){
+        return propNames;
+    }
+}
+
 function getAllData(url){
     return fetch(url).then(res => res.text()).then(body => JSON.parse(body));
 }
 
+/**
+ * 
+ * @param {Array<object>} games 
+ * @param {String} id 
+ */
+function searchFromId(games, id){
+    return games.map(value => filter(value, id))
+}
 
-getAllData('https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&client_id='+CLIENT_ID).then(str => console.log(str));
+getAllGames('https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&client_id='+CLIENT_ID).then(games => console.log(searchFromId(games, 'RLlDWHh7hR')));
